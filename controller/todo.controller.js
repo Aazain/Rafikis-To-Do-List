59faeb1abe200a94c0d21bf1ec7b87e0481a34b5
Aazain <aazainrafiki@gmail.com>
2020-12-26 20:27:19 -0500

    finished get and post routes, need to do editing route and fix delete route.

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,24 +1,35 @@
+const Items = require("../model/todo.model")
 
 module.exports = (app) => {
     app.get("/", function(req,res){
         const findData = Items.find(function(err,foundData){
+            if (err) {
+                res.status(400).send({message: "Error getting todo list"})
+            }
             return res.send({
                 foundData
-        })
-    })        
-})
-    
-    app.post("/", function(req,res){
-        const itemList = ({
-            name: req.body.name
-        })
-        itemList.save(function(err){
+            })
+         })    
+        });  
+
+    app.delete("/", function(req,res){
+        Items.deleteOne(
+            {_id: "5fe7e285a161e441dc78f0b2"}, 
+        function(err){
             if (!err){
-                res.send("Successfully added to DB")
+                res.send("Successfully Deleted Task")
             }else{
-                res.send("Failed to add name to DB")
+                console.log("Failed to Delete List Task")
             }
         })
+    });
+
+    app.post("/", function(req,res){
+        const itemList = new Items({
+            name: req.body.name
+        })
+        itemList.save()
     })
+
 }
 
--- model/todo.model.js
+++ model/todo.model.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-module.exports = (app) => {
 const listSchema = new mongoose.Schema({
     name: String
 })
 const Items = mongoose.model("Items", listSchema)
-}
+
+
+module.exports = Items;
 
 
