7cf89d99f1f791e69f5afb1605f68daaa2a720c4
Aazain <aazainrafiki@gmail.com>
2020-12-28 18:15:46 -0500

    Almost done backend, need to fix error codes etc...

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -2,60 +2,88 @@ const Items = require("../model/todo.model")
 
 module.exports = (app) => {
 
-    app.get("/todo", function(req,res){
-        const findData = Items.find(function(err,foundData){
-            if (err) {
-                res.status(400).send({message: "Error getting todo list"})
-            }
-            return res.send({
-                foundData
-            })
-         })
-        });  
-    
-    app.delete("/todo/:id", function(req,res){
-        const {id} = req.params
+    app.get("/todo/:id", function(req, res) {
+        const {
+            id
+        } = req.params;
         console.log(id)
-        Items.findByIdAndRemove(
-            {_id: id},
-        function(err){
-            if (!err){
-                res.send("Successfully Deleted Task")
-            }else{
-                res.send("Failed to Delete List Task")
+        Items.findById(id, function(err, foundData) {
+            if (err) {
+                res.status(404)
+                res.send("Task not found")
+            } else {
+                res.send(foundData)
             }
+        })
+
+    });
+
+    app.get("/todo", function(req, res) {
+        const findData = Items.find(function(err, foundData) {
+            if (err) {
+                res.status(400).send({
+                    message: "Error getting todo list"
+                })
+            }
+            return res.send([
+                ...foundData
+            ])
         })
     });
 
-    app.post("/todo", function(req,res){
+    app.delete("/todo/:id", function(req, res) {
+        const {
+            id
+        } = req.params
+        console.log(id)
+        Items.findByIdAndRemove({
+                _id: id
+            },
+            function(err, result) {
+                if (!err) {
+                    res.send("Successfully Deleted Task")
+                } else {
+                    res.status(500).send("Unable to delete task, ")
+                }
+            })
+    });
+
+    app.post("/todo", function(req, res) {
         const itemList = new Items({
             name: req.body.name,
             status: req.body.status
         })
-        itemList.save()
+        itemList.save().then(() => {
+                return res.send("Successfully added task to list");
+            })
+            .catch((err) => {
+                res.status(500)
+            })
     })
 
-    app.patch("/todo/:id", function(req,res){
-        const {id} = req.query;
+    app.patch("/todo/:id", function(req, res) {
+        const {
+            id
+        } = req.params;
+        const {
+            name
+        } = req.body;
         console.log(id);
-        Items.updateOne(
-            {id},
-            {$set: {name: req.body}},
-            function(err){
-                if(!err){
+        Items.updateOne({
+                _id: id
+            }, {
+                $set: {
+                    name
+                }
+            },
+            function(err) {
+                if (!err) {
                     res.send("Successfully edited Task")
-                } else{
+                } else {
                     res.send("Failed to edit task")
                 }
             }
         )
 
     })
-    
 }
-
-
-
-
-
-
--- model/todo.model.js
+++ model/todo.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const listSchema = new mongoose.Schema({
     name: String,
     status: Boolean
-},{timestamps: true})
+}, {
+    timestamps: true
+})
 const Items = mongoose.model("Items", listSchema)
 
 
