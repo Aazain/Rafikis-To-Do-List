7d29c53e9f4fb868e6b8f95c5bd35c4d5dd581e9
Aazain <aazainrafiki@gmail.com>
2020-12-28 18:15:46 -0500

    Almost done backend, need to fix error codes etc...

--- myBackend/server.js
+++ myBackend/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
-const bodyparser = require ("body-parser")
+const bodyparser = require("body-parser")
 const todoController = require("./controller/todo.controller");
 const todoModel = require("./model/todo.model")
 const mongoose = require('mongoose');
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.urlencoded({
+    extended: false
+}))
 require('../myBackend/node_modules/dotenv/config')
-mongoose.connect(process.env.DB_PASS, {useNewUrlParser: true, useUnifiedTopology: true}, function(req,res){console.log("Connected to DB...")});
+mongoose.connect(process.env.DB_PASS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, function(req, res) {
+    console.log("Connected to DB...")
+});
 
 todoController(app);
 
 
-app.listen(3000, function(req,res){
+app.listen(3000, function(req, res) {
     console.log("Server started on port 3000")
 })
-
