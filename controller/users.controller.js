cae10635b1694ae0f059181d74b256a97cb4b145
Aazain <aazainrafiki@gmail.com>
2021-02-06 18:40:38 -0500

    started to implement JWT, need to create middleware/ authentication.

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const Users = require("../model/users.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+app.use(express.json());
 
 
 module.exports = (app)=>{
 
     app.get("/users", function(req, res) {
+        //Find all users and then return them
         const findUsers = Users.find(function(err, foundUsers) {
             if (err) {
                 res.status(400).send({
@@ -22,11 +26,14 @@ module.exports = (app)=>{
 
     app.post("/users", async function(req, res){
         try{
+            //Hashes Password from password body
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
             const userList = new Users({
+                name: req.body.name,
                 email: req.body.email,
                 password: hashedPassword
             });
+            //Saves Hashed password
             userList.save()
             return res.status(201).send("Successfully added user");
             }
@@ -36,20 +43,30 @@ module.exports = (app)=>{
     })
 
     app.post("/users/login", async function(req, res){
-        console.log("tyest")
+
+        const username = req.body.email;
+        const currentUser = username;
+
+        // Looks to see if user input exists in database
         let user;
              user = await Users.findOne({
                 email: req.body.email
             })
-      
+
+        //if user does not exist, return error code
         if(user == null){
             return res.status(400).send("Canoot find user")
         }
+
+        //Bycrypt compares password input to hashed password and if it matches, login is successfull
             return bcrypt.compare(req.body.password, user.password, function (err) {
                 if (err) {
                     return res.send({message: "Incorrect Email or Password"})
                 } else {
-                    return res.send({message: "Success"})
+                    
+                    const accessToken = jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET)
+                    res.json({accessToken: accessToken});
+                    
                 }
             })
     })
