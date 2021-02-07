eee3b76abbac7115fc44c7ec74bf965aa25128bd
Aazain <aazainrafiki@gmail.com>
2021-02-06 22:41:19 -0500

    Implemented JWT successfully, need to find a way to render list items based on logged in user, refresh tokens and logouts.

--- myBackend/controller/users.controller.js
+++ myBackend/controller/users.controller.js
@@ -29,7 +29,6 @@ module.exports = (app)=>{
             //Hashes Password from password body
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
             const userList = new Users({
-                name: req.body.name,
                 email: req.body.email,
                 password: hashedPassword
             });
@@ -42,33 +41,91 @@ module.exports = (app)=>{
             }
     })
 
-    app.post("/users/login", async function(req, res){
+   
+    const poster = app.post("/users/login", async function(req, res){
 
-        const username = req.body.email;
-        const currentUser = username;
-
-        // Looks to see if user input exists in database
+        const currentUser = req.body.email;
         let user;
+        // Looks to see if user input exists in database
              user = await Users.findOne({
                 email: req.body.email
             })
+            
 
         //if user does not exist, return error code
         if(user == null){
-            return res.status(400).send("Canoot find user")
+            return res.status(400).send("Incorrect Email or Password")
         }
 
         //Bycrypt compares password input to hashed password and if it matches, login is successfull
-            return bcrypt.compare(req.body.password, user.password, function (err) {
-                if (err) {
-                    return res.send({message: "Incorrect Email or Password"})
+            return bcrypt.compare(req.body.password, user.password, function (err, result) {
+                if (!result) {
+                    res.status(203).send("Incorrect Email or Password") 
                 } else {
-                    
                     const accessToken = jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET)
-                    res.json({accessToken: accessToken});
-                    
+                    res.json({accessToken: accessToken})
                 }
             })
+         
     })
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This code is functional. It takes the JWT and checks if any array items have the same
+// {email:} as the email of the JWT and then it renders the array items that include it.
+
+
+// function tokenAuth(req,res,next){
+//     const authHeader = req.headers['authorization']
+//     const token = authHeader && authHeader.split(' ')[1];
+//     if(token == null) return res.sendStatus(401)
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//         if (err) return res.status(403)
+//         req.user = user
+//         next();
+//     })}
+
+//     app.get("/test", tokenAuth, function(req, res){
+//         res.json(post.filter(post => post.email === req.user ))
+//     })
+
+//     const post = [
+//         {
+//             email: "aazain@gmail.com",
+//             title: "cool"
+//         },
+//         {
+//             email:"aazainman@gmail.com",
+//             title:"good job"
+//         },
+//         {
+//             email:"aazainman@gmail.com",
+//             title:"good job"
+//         },
+//         {
+//             email:"aazainman@gmail.com",
+//             title:"good job"
+//         },
+//         {
+//             email:"aazainman@gmail.com",
+//             title:"good job"
+//         }
+        
+//     ];
