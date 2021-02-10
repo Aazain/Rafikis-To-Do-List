b3f9cbb807ef7774f4a478fa6e64f24e83d5fbc9
Aazain <aazainrafiki@gmail.com>
2021-02-09 20:25:12 -0500

    updated jwt middleware on app.get for to do list items sso only a logged in users item shows

--- myBackend/controller/users.controller.js
+++ myBackend/controller/users.controller.js
@@ -90,19 +90,10 @@ module.exports = (app)=>{
 
 
     function newToken(currentUser){
-        return jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "20s"})
+        return jwt.sign(currentUser, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30s"})
     }
 
-    function tokenAuth(req,res,next){
-        const authHeader = req.headers['authorization']
-        const token = authHeader && authHeader.split(' ')[1];
-        if(token == null) return res.sendStatus(401)
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) return res.status(403).send("Forbidden")
-            req.user = user
-            next();
-        })
-    }
+
 }
 
 
