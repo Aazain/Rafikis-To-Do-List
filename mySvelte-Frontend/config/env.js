b355b98c6eca2090d41d2a1b42d90cd716255604
Aazain <aazainrafiki@gmail.com>
2021-02-26 18:50:33 -0500

    changes heroku server location

--- mySvelte-Frontend/config/env.js
+++ mySvelte-Frontend/config/env.js
@@ -6,7 +6,7 @@ export function env() {
     if(location.hostname === "localhost"){ 
         return "http://localhost:4000"
     }else{
-        return "https://rafikis-todo-server.herokuapp.com/"
+        return "https://rafikis-todo-server.herokuapp.com"
     }
 
 }
