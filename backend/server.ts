839cb7a5c3a63fc9b406e6445b78e64df31b3271
Aazain <aazainrafiki@gmail.com>
2021-06-24 18:39:49 -0400

    heroku commit

--- backend/server.ts
+++ backend/server.ts
@@ -16,7 +16,7 @@ app.use(cors({origin: "*"}));
 app.options('*', cors()) ;
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
