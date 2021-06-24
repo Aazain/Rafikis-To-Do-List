d9920c856caf278368de339cde4de470da29f0eb
Aazain <aazainrafiki@gmail.com>
2021-06-24 18:54:03 -0400

    heroku commit

--- dist/models/users.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Users = void 0;
-var mongoose_1 = require("mongoose");
-var userSchema = new mongoose_1.Schema({
-    email: String,
-    password: String
-});
-exports.Users = mongoose_1.model("Users", userSchema);
