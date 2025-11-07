5cab8f37f5e3ad47c64b6e8d84e663acfdeb912b
Aazain <uhzaynraf@gmail.com>
2025-11-06 22:07:20 -0500

    updated backend link

--- frontend/rafikis-to-do-list/src/config/env.ts
+++ frontend/rafikis-to-do-list/src/config/env.ts
@@ -3,6 +3,6 @@ export function env() {
     if(location.hostname === "localhost"){ 
         return "http://localhost:4000"
     }else{
-        return "https://rafikis-to-do-list.onrender.com/"
+        return "https://rafikis-to-do-list.onrender.com"
     }
 }
